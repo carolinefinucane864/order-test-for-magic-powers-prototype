af51c1a62a2183d11f4cf0f2d25b5e4c59927b18
davekingdesign <dave.king4@nhs.net>
2025-09-22 10:35:37 +0000

    Set name of the service

--- app/config.js
+++ app/config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   // Service name
-  serviceName: 'Service name goes here',
+  serviceName: 'Order a test to see if you have magical powers',
 
   // Port to run nodemon on locally
   port: 2000,
